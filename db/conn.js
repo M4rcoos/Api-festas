49b23448232eedd6e0de865fc6666972fe8a38a5
M4rcoos <contato.shopvmax@gmail.com>
2023-11-30 09:46:49 -0300

    fix: correcting moongoose

--- db/conn.js
+++ db/conn.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 async function main(){
   try{
     await mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser: true, useUnifiedTopology: true
 })
   }catch(error){
     console.log(`erro:${error}` )
